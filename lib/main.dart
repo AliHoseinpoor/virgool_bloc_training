d4c8b949fd5bef8af2305475e5016f109e4cef24
Ali <iamhoseinpoor@gmail.com>
2021-01-03 15:19:38 +0330

    some change in gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:virgool_bloc_learn/bloc/counter_cubit.dart';
 import 'package:virgool_bloc_learn/bloc/login/check_login_cubit.dart';
 import 'package:virgool_bloc_learn/page/login_page.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context)=>CheckLoginCubit(),
+      create: (context) => CheckLoginCubit(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -19,6 +20,38 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: LoginPage(),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  final CounterCubit counterCubit = CounterCubit();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Push The Button For Change the Number'),
+            const SizedBox(height: 10),
+            BlocBuilder<CounterCubit, int>(
+              cubit: counterCubit,
+              builder: (context, int state) {
+                return Text(
+                  state.toString(),
+                  style: TextStyle(fontSize: 40),
+                );
+              },
+            ),
+            const SizedBox(height: 10),
+            RaisedButton(onPressed: () => counterCubit.increment(), child: Text('Increment')),
+            const SizedBox(height: 1),
+            RaisedButton(onPressed: () => counterCubit.decrement(), child: Text('Decrement')),
+          ],
+        ),
       ),
     );
   }
